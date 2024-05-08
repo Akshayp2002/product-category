866f1a90d1b923248167b162e138aecf27ad8f5e
Akshay <akshay@cloudrevelinnovation.com>
2024-05-08 15:14:33 +0530

    login page

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\Admin\UserController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return to_route('login');
 });
 
 Route::get('/dashboard', function () {
